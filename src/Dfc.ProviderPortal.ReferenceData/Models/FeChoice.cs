79a092b4d24a58e19b11144004e4fb5c0ebcfee3
Steve O'Hara <github@soharadesign.com>
2020-03-24 15:22:49 +0000

    [COUR-2342] Fixed error when encountering null FeChoices scores

--- src/Dfc.ProviderPortal.ReferenceData/Models/FeChoice.cs
+++ src/Dfc.ProviderPortal.ReferenceData/Models/FeChoice.cs
@@ -7,8 +7,8 @@ namespace Dfc.ProviderPortal.ReferenceData.Models
     {
         public Guid Id { get; set; }
         public int UKPRN { get; set; }
-        public decimal LearnerSatisfaction { get; set; }
-        public decimal EmployerSatisfaction { get; set; }
+        public decimal? LearnerSatisfaction { get; set; }
+        public decimal? EmployerSatisfaction { get; set; }
         public DateTime? CreatedDateTimeUtc { get; set; }
     }
 }
